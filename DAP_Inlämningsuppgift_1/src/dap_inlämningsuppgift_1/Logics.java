9fa6a22640a621b19a9d0b960a60209206b845f8
Bujamin Saliu <bujamin.saliu@gmail.com>
2017-05-02 18:07:51 +0200

    changed pics

--- DAP_Inlämningsuppgift_1/src/dap_inlämningsuppgift_1/Logics.java
+++ DAP_Inlämningsuppgift_1/src/dap_inlämningsuppgift_1/Logics.java
@@ -23,32 +23,37 @@ public class Logics {
          artistArrayList = new ArrayList<>();
          albumArrayList = new ArrayList<>();
         Album album1 = new Album(1, "Thriller", 1990);
-        Album album2 = new Album(2, "Thriller2", 1992);
-        Album album3 = new Album(3, "Thriller3", 1993);
+        Album album2 = new Album(2, "21", 2016);
+        Album album3 = new Album(3, "The Bodyguard", 1993);
+        Album album4 = new Album(4, "Supernatural", 1993);
         
-        Artist artist1 = new Artist(1, "Michael", "tackson");
-        
+        Artist artist1 = new Artist(1, "Michael", "Jackson");
         artist1.getAlbumArrayList().add(album1);
-        artist1.getAlbumArrayList().add(album2);
-        
-        
-        Artist artist2 = new Artist(2, "nichael2", "kackson2");
+               
+        Artist artist2 = new Artist(2, "Adele", " ");
         artist2.getAlbumArrayList().add(album2);
-        Artist artist3 = new Artist(3, "qichael3", "wackson3");
+        
+        Artist artist3 = new Artist(3, "Whitney", "Houston");
         artist3.getAlbumArrayList().add(album3);
+        
+        Artist artist4 = new Artist(4, "Santana", " ");
+        artist4.getAlbumArrayList().add(album4);
         
         artistArrayList.add(artist1);
         artistArrayList.add(artist2);
         artistArrayList.add(artist3);
+        artistArrayList.add(artist4);
         
         album1.getArtistArrayList().add(artist1);
         album2.getArtistArrayList().add(artist2);
         album2.getArtistArrayList().add(artist1);
         album3.getArtistArrayList().add(artist3);
+        album4.getArtistArrayList().add(artist4);
      
         albumArrayList.add(album1);
         albumArrayList.add(album2);
         albumArrayList.add(album3);
+        albumArrayList.add(album4);
         
     }
     
